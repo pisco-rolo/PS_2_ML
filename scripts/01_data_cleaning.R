8b63d7bae94f4ba45753f9fd19c542281fbc82c6
Julián Rojas <Judrojasag@unal.edu.co>
2023-10-06 17:04:18 -0500

    Update 01_data_cleaning.R
    Importar los datos.

---
+++ scripts/01_data_cleaning.R
@@ -0,0 +1,3 @@
+# 1| Importar -------------------------------------------------------------
+dataset <- read.csv(file = paste0(directorioDatos, 'train.csv'))
+dataset_kaggle <- read.csv(file = paste0(directorioDatos, 'test.csv'))
