70da9945c22ee8cea046d7f0f2f977f43cf90578
Julián Rojas <Judrojasag@unal.edu.co>
2023-10-22 22:29:10 -0500

    Fix elastic net
    Disminuir el número de modelos sobre los que evaluar los hiperparámetros.
    
    Co-Authored-By: CAMILOOBANDO97 <42219653+CAMILOOBANDO97@users.noreply.github.com>

--- scripts/05_forecast.R
+++ scripts/05_forecast.R
@@ -3,10 +3,12 @@
 # sean consistentes. Si la variable inicia con 'cat_', indica que es 
 # categórica y ello facilita la implementación del modelo.
 dataset <- dataset |> 
-  rename(cat_localidad = nom_localidad)
+  rename(cat_localidad = nom_localidad) |> 
+  mutate(across(c(num_mt2, num_bano), ~replace_na(., mean(., na.rm=TRUE)))) 
 
 dataset_kaggle <- dataset_kaggle |> 
-  rename(cat_localidad = nom_localidad)
+  rename(cat_localidad = nom_localidad) |> 
+  mutate(across(c(num_mt2, num_bano), ~replace_na(., mean(., na.rm=TRUE)))) 
 
 # 2| Predicción -----------------------------------------------------------
 # 2.1| xgboost ------------------------------------------------------------
@@ -226,9 +228,9 @@ if (primeraVez == TRUE) {
 # incluidos en elastic net, preferimos usar elastic net.
 tune_grid_elasticNet <- grid_regular(
   # La penalización va desde 0.0001 hasta 1,000.
-  penalty(range = c(-4, 3), trans = log10_trans()), # Relacionado con la penalización a la función de pérdida.
+  penalty(range = c(0.001, 1000), trans = NULL), # Relacionado con la penalización a la función de pérdida.
   mixture(range = c(0, 1), trans = NULL), # Relacionado con la ponderación a Lasso.
-  levels = c(penalty = 100, mixture = 10)
+  levels = c(penalty = 20, mixture = 10)
 )
 
 elasticNet_model <- linear_reg(
@@ -238,30 +240,32 @@ elasticNet_model <- linear_reg(
   set_mode('regression') |> 
   set_engine('glmnet')
 
-
 recipe_elasticNet <- recipe(num_precio ~ .,
-    data = dataset |> 
-    mutate(across(c(num_mt2, num_bano), ~replace_na(., mean(., na.rm=TRUE)))) |> 
-    st_drop_geometry()) |> 
-  
+                            data = dataset |> 
+                              st_drop_geometry()) |> 
   update_role(id_hogar, new_role = 'ID') |> 
+  # Una muestra de entrenamiento puede no tener todas las localidades, por lo 
+  # que es necesario que se asigne categorías anteriormente no vistas a la
+  # categoría 'new'.
+  step_novel(all_nominal_predictors()) |> 
+  step_poly(num_distancia_calles, num_distancia_mall, num_distancia_tm, degree = 2) |>
   step_dummy(all_nominal_predictors()) |> 
-  #step_interact(terms = ~ bin_zonaResidencial:bin_parqueadero +
-   #               starts_with("cat_estrato"):bin_zonaLaboral + 
-    #              starts_with("cat_estrato"):starts_with("cat_localidad")+
-     #             bin_casa:bin_parqueadero+num_mt2:starts_with("cat_estrato")+
-      #            num_mt2:starts_with("cat_localidad")+num_mt2:num_piso)|>
-  #step_normalize(all_predictors()) |> 
-  step_poly(num_distancia_calles, degree = 2) |>
-  step_poly(num_distancia_mall, degree = 2) |>
-  step_poly(num_distancia_tm, degree = 2)
-  
+  step_zv(all_predictors()) |> 
+  # TODO. La interacción no la hemos podido hacer funcionar.
+  step_interact(terms = ~ bin_zonaResidencial:bin_parqueadero +
+                  # starts_with("cat_estrato"):bin_zonaLaboral +
+                  # starts_with("cat_estrato"):starts_with("cat_localidad") +
+                  bin_casa:bin_parqueadero
+                  # num_mt2:starts_with("cat_estrato") +
+                  # num_mt2:starts_with("cat_localidad") +
+                  # num_mt2:num_piso
+  ) |>
+  step_normalize(all_double_predictors())
   
 wf_elasticNet <- workflow() |> 
   add_recipe(recipe_elasticNet) |> 
   add_model(elasticNet_model)
 
-primeraVez = TRUE
 if (primeraVez == TRUE) {
   tune_elasticNet <- tune_grid(
     wf_elasticNet,
@@ -312,7 +316,35 @@ if (primeraVez == TRUE) {
 # 2.4| Ensemble -----------------------------------------------------------
 # Los 3 modelos anteriores tienen ventajas y desventajas. Sin embargo, es 
 # posible combinar las bondades de las diferentes predicciones mediante una
-# combinación de pronósticos.
+# combinación de pronósticos. Por ello, creamos una base de datos que
+# contiene las predicciones de los tres modelos anteriores.
+dataset_ensemble <- tibble(
+  id_hogar = dataset$id_hogar,
+  num_precio = dataset$num_precio,
+  xgboost = predict(definitive_xgboost_fit, new_data = dataset) |> _$.pred,
+  lightgbm = predict(definitive_lightgbm_fit, new_data = dataset) |> _$.pred,
+  elasticNet = predict(definitive_elasticNet_fit, newData = dataset) |> _$.pred
+)
+
+dataset_kaggle_ensemble <- tibble(
+  id_hogar = dataset_kaggle$id_hogar,
+  xgboost = predict(definitive_xgboost_fit, new_data = dataset_kaggle) |> _$.pred,
+  lightgbm = predict(definitive_lightgbm_fit, new_data = dataset_kaggle) |> _$.pred,
+  elasticNet = predict(definitive_elasticNet_fit, newData = dataset_kaggle) |> _$.pred
+)
+
+# A las predicciones es importante agregarles su dimensión espacial, pues
+# la validación por bloques necesita del sentido de contiguidad.
+dataset_ensemble <- inner_join(x = dataset_ensemble, 
+                               y = dataset |> select(c('id_hogar'))) |> 
+  st_as_sf()
+
+# Ponemos una semilla adicional, pues pasos anteriores pueden haber tenido un
+# impacto en el generador de números pseudo-aleatorios. Así mismo, realizamos
+# una validación espacial por bloques, incluyendo cinco folds.
+set.seed(123)
+block_folds <- spatial_block_cv(dataset_ensemble, v = 5)
+
 # Nota. La forma automática toma una combinación lineal de todos los modelos
 # posibles -es decir, incluyendo cualquier combinación entre los 
 # hiperparámetros-. Sin embargo, ello no parece adecuado. Por consiguiente, a
@@ -320,4 +352,72 @@ if (primeraVez == TRUE) {
 # la mejor combinación de hiperparámetros.
 # Definimos la grilla donde se buscarán los hiperparámetros que maximizan el
 # pronóstico por fuera de muestra.
-tune_grid_emsemble <- grid_regular()
+tune_grid_emsemble <- grid_regular(
+  # La penalización va desde 0.0001 hasta 1,000.
+  penalty(range = c(-4, 3), trans = log10_trans()), # Relacionado con la penalización a la función de pérdida.
+  mixture(range = c(0, 1), trans = NULL), # Relacionado con la ponderación a Lasso.
+  levels = c(penalty = 20, mixture = 10)
+)
+
+ensemble_model <- linear_reg(
+  penalty = tune(),
+  mixture = tune()
+) |> 
+  set_mode('regression') |> 
+  set_engine('glmnet')
+
+recipe_ensemble <- recipe(num_precio ~ ., 
+                          data = dataset_ensemble |> st_drop_geometry()) |> 
+  update_role(id_hogar, new_role = 'ID')
+
+wf_ensemble <- workflow() |> 
+  add_recipe(recipe_ensemble) |> 
+  add_model(ensemble_model)
+
+if (primeraVez == TRUE) {
+  tune_ensemble <- tune_grid(
+    wf_ensemble,
+    resamples = block_folds,
+    grid = tune_grid_ensemble,
+    metrics = metric_set(mae)
+  )
+  
+  saveRDS(object = tune_ensemble,
+          file = paste0(directorioDatos, 'optim_parms_ensemble.rds'))
+  
+  best_parms_ensemble <- select_best(tune_ensemble, metric = 'mae')
+  definitive_ensemble <- finalize_workflow(
+    x = wf_ensemble,
+    parameters = best_parms_ensemble
+  )
+  
+  definitive_ensemble_fit <- fit(object = definitive_ensemble,
+                                 data   = dataset_ensemble)
+  
+  prediccion <- tibble(
+    id_hogar = dataset_ensemble$id_hogar,
+    precio_obs = dataset_ensemble$num_precio,
+    precio_est = predict(definitive_ensemble_fit, new_data = dataset_ensemble) |> 
+      _$.pred
+  )
+  
+  tune_elasticNet |> show_best(metric = 'mae', n = 5) 
+  
+  prediccion <- tibble(
+    property_id = dataset_kaggle$id_hogar,
+    price = predict(definitive_ensemble_fit, new_data = dataset_kaggle_ensemble) |> 
+      _$.pred
+  )
+  
+  # Nota. Dejamos comentada la exportación para no modificar el archivo que ya
+  # publicamos en Kaggle.
+  write.csv(x = prediccion,
+            file = paste0(directorioResultados, 'ensemble_imp1_hip1.csv'),
+            row.names = FALSE)
+  
+} else {
+  tune_elasticNet <- readRDS(file = paste0(directorioDatos,
+                                           'optim_parms_ensemble.rds'))
+  prediccion_elasticNet <- read.csv(file = paste0(directorioResultados, 
+                                                  'ensemble_imp1_hip1.csv'))
+}
